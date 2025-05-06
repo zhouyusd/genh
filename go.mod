b4afc68781ccb501542c83807da28826712cfd22
zhouyusd <1848611319@qq.com>
2025-05-06 18:43:08 +0800

    feat: add JWT token generation and parsing functionality

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 toolchain go1.24.2
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	golang.org/x/sync v0.13.0
 	golang.org/x/tools v0.32.0
 )
---
+++ jwt.go
@@ -0,0 +1,40 @@
+package genh
+
+import (
+	"sync"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	globalSecret []byte
+	once         sync.Once
+)
+
+type UserClaims struct {
+	UserID   int    `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenTokenWithHS256(claims *UserClaims, secret []byte) (string, error) {
+	once.Do(func() {
+		globalSecret = secret
+	})
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(globalSecret)
+}
+
+func ParseTokenWithHS256(tokenStr string) *UserClaims {
+	if globalSecret == nil {
+		return nil
+	}
+	claims := &UserClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return globalSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil
+	}
+	return claims
+}
