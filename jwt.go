6caf170c7ba1f72f4629dba65bcbe3042673de8c
zhouyusd <1848611319@qq.com>
2025-07-08 00:07:39 +0800

    feat: add ParseContextWithHS256 function for JWT token parsing

--- jwt.go
+++ jwt.go
@@ -1,6 +1,7 @@
 package genh
 
 import (
+	"strings"
 	"sync"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -26,6 +27,22 @@ func GenTokenWithHS256(claims *UserClaims, secret []byte) (string, error) {
 }
 
 func ParseTokenWithHS256(tokenStr string) *UserClaims {
+	if globalSecret == nil {
+		return nil
+	}
+	claims := &UserClaims{}
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return globalSecret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil
+	}
+	return claims
+}
+
+func ParseContextWithHS256(h interface{ Header(string) string }) *UserClaims {
+	tokenStr := h.Header("Authorization")
+	tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
 	if globalSecret == nil {
 		return nil
 	}
